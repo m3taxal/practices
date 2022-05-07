f6ec139a7b1cd3c427907c100645478d88e349e3
metaxal <ayturfarhadi@outlook.de>
2022-05-07 16:50:18 +0200

    learned some OOP

---
+++ src/com/practices/mainProgram/Main.java
@@ -0,0 +1,29 @@
+package com.practices.mainProgram;
+
+import com.practices.objectOriented.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person ally = new Person("ally");
+    }
+}
+/*
+public class Example {
+    public static void main(String[] args) {
+        Person first = new Person("First");
+
+        System.out.println(first);
+        youthen(first);
+        System.out.println(first);
+
+        Person second = first;
+        youthen(second);
+
+        System.out.println(first);
+    }
+
+    public static void youthen(Person person) {
+        person.setBirthYear(person.getBirthYear() + 1);
+    }
+}
+ */
---
+++ src/com/practices/objectOriented/Person.java
@@ -0,0 +1,24 @@
+package com.practices.objectOriented;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String initialName) {
+        this.age = 0;
+        this.name = initialName;
+    }
+
+    public void printPerson() {
+        System.out.println(this.name + ", age " + this.age + " years");
+    }
+
+    public void growOlder(){
+        this.age += 1;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
